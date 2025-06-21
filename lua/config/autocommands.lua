61ebd4773d8dd6dcb00f135b5ef658c6804bf08d
Alessandro Rocco Scisca <alessandrorocco.scisca@arm.com>
2025-06-21 10:13:20 +0200

    Add treesitter

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -4,3 +4,28 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.o.tabstop = 2
   end
 })
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  callback = function()
+    local ts = require('nvim-treesitter')
+
+    -- Is this buffer too big?
+    local fname = vim.api.nvim_buf_get_name(0)
+    local fsize = vim.fn.getfsize(fname)
+    local maxsize = 1024 * 1024  -- 1 MiB
+    if fsize > maxsize then
+        return
+    end
+
+    -- Is the parser installed?
+    local installed = ts.get_installed('parsers')
+    if not vim.list_contains(installed, vim.bo.filetype) then
+      return
+    end
+
+    -- Start treesitter and use its power.
+    vim.treesitter.start()
+    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+})
