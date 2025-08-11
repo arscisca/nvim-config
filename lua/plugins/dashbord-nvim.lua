13a6f3afbe8adb81b7d198eeb79809f4e60a59bd
alesci01 <alessandrorocco.scisca@arm.com>
2025-08-11 11:32:26 +0200

    Update dashboard

--- lua/plugins/dashbord-nvim.lua
+++ lua/plugins/dashbord-nvim.lua
@@ -24,11 +24,14 @@ return {
         [[         `'                                     ]],
       },
       project = { enable = false, limit = 0 },
-      mru = { enable = true, limit = 9 },
+      mru = { enable = true, limit = 9, cwd_only = true},
       packages = { enable = true },
       footer = {
         "Made with <3",
-      }
+      },
+      shortcut = {
+        {desc = 'Open file tree', key = 'e', action = function() require('neo-tree.command').execute({}) end},
+      },
     },
   },
   dependencies = { {'nvim-tree/nvim-web-devicons'}}
