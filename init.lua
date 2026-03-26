5a7a926bfa9b76a6d4454056dde1c2af6afe5bf3
Alessandro Rocco Scisca <alessandrorocco.scisca@arm.com>
2026-03-26 12:40:25 +0000

    Allow local config extension
    Allow to add configuration extensions on a per-project and per-machine
    basis for stuff that is too specific to commit to this repo.

--- init.lua
+++ init.lua
@@ -3,3 +3,7 @@ require('config.colorscheme')
 require('config.autocommands')
 require('config.keymaps')
 require('config.commands')
+
+-- Add the possibility of having local overrides outside of this repo.
+vim.opt.runtimepath:append("vim.fn.getcwd()" .. "/.nvim")               -- Per project.
+vim.opt.runtimepath:append(os.getenv("HOME") .. "/.config/nvim/local")  -- Per machine.
